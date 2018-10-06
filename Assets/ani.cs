81d1d12f239dfddb75d540d462aebd34dac9a778
Vanden Eynde <Sam.VandenEynde@ordina.be>
2018-10-06 17:51:27 +0200

    Saved scene

--- Assets/ani.cs
+++ Assets/ani.cs
@@ -3,14 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ani : MonoBehaviour {
-    public Animation anim;
+    public Animator anim;
 	// Use this for initialization
 	void Start () {
-        anim = GetComponent<Animation>();
-        foreach(AnimationState state in anim)
-        {
-            state.speed = 0.5F;
-        }
+        anim = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
